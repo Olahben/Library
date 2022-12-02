2b80b559ea6eae07fae350afc8f5dbe32260e461
Ola <ohagenbendixen@gmail.com>
2022-12-02 17:53:01 +0100

    Remove unnecessary content from addBookToLibrary function

--- app.js
+++ app.js
@@ -10,11 +10,7 @@ function Book(title, author, pages, haveRead) {
 }
 
 function addBookToLibrary() {
-    const newBook = new Book('Heksene', 'Roald Dahl', 200, true)
-    const newBook2 = new Book('The richest man in babylon', 'Ola', 315, false)
-    const newBook3 = new Book('RIch dad poor dad', 'Robert', 300, false)
-    const newBook4 = new Book('Neuroplasticity', 'Andrew', 489, false)
-    myLibrary.push()
+    myLibrary.push(book)
 }
 addBookToLibrary()
 
