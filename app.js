3d1555af4a5ef78e2b14409556a424a6f3e56278
Ola <ohagenbendixen@gmail.com>
2022-12-02 17:46:31 +0100

    Add new books

--- app.js
+++ app.js
@@ -11,6 +11,9 @@ function Book(title, author, pages, haveRead) {
 
 function addBookToLibrary() {
     const newBook = new Book('Heksene', 'Roald Dahl', 200, true)
+    const newBook2 = new Book('The richest man in babylon', 'Ola', 315, false)
+    const newBook3 = new Book('RIch dad poor dad', 'Robert', 300, false)
+    const newBook4 = new Book('Neuroplasticity', 'Andrew', 489, false)
     myLibrary.push(newBook)
 }
 addBookToLibrary()
