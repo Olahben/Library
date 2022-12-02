17bd58ed9f4a1aefae49d710a54435f01c4b6b91
Ola <ohagenbendixen@gmail.com>
2022-12-02 19:23:25 +0100

    Create function that creates the book card

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 let myLibrary = [new Book('Heksene', 'Roald Dahl', 200, true ), new Book('The richest man in babylon', 'Ola', 315, false), new Book('RIch dad poor dad', 'Robert', 300, false), new Book('Neuroplasticity', 'Andrew', 489, false)]
 
+function createBookCard() {
+    const div = document.createElement('div')
+    container.appendChild(div)
+    div.classList.add('card')
+}
+
 function Book(title, author, pages, haveRead) {
     this.title = title, 
     this.author = author, 
@@ -10,7 +16,7 @@ function Book(title, author, pages, haveRead) {
 }
 
 function addBookToLibrary() {
-    myLibrary.push(book)
+    myLibrary.push()
 }
 addBookToLibrary()
 
@@ -19,3 +25,4 @@ function displayLibrary() {
 }
 
 displayLibrary()
+
